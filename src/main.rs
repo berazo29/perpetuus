575448c19b8e1b6bc5ce18ab9a15f1f5faf5b87e
berazo29 <37014465+berazo29@users.noreply.github.com>
2024-01-15 23:18:24 -0500

    feat: add fire endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/fire")]
+fn fire() -> &'static str {
+    "🚀"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, fire])
 }
